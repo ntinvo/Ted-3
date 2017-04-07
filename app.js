c8319bd336b43077f17f1e42cc74ea52dff14940
Tin Vo <tinnvo1101@gmail.com>
2017-04-07 16:46:37 -0500

    Can add users to database now

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express       = require('express');
 const log           = require('morgan');
 const bodyParser    = require('body-parser');
+const usersList     = require('./users').arr;
 
 // set up express app
 const app = express();
@@ -29,21 +30,53 @@ sequelize
     console.log('Unable to connect to the database:', err);
   });
 
+var Users = sequelize.define('Users', {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  address: Sequelize.STRING
+});
+
+sequelize
+  .sync({force:true})
+  .then(function(err) {
+    usersList.forEach(function(user) {
+      var userInstance = Users.build({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        address: user.address
+      });
+      userInstance.save();
+    });
+    console.log('It worked.')
+  }, function (err) {
+    console.log('An error occurred!!!', err);
+  });
+
+
+
 // parse the requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-// initial set up - catch all routes
-app.get('*', (req, res) => res.status(200).send({
+// initial set up - catch root route
+app.get('/', (req, res) => res.status(200).send({
     message: 'Welcome to TED',
 }));
 
-// root route
-app.get('/',function(req,res){
-	res.sendFile(path.join(__dirname + '/index.html'));
-});
+
+
+//
+// // root route
+// app.get('/',function(req,res){
+// 	res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+
+
 
 // module.exports = app;
 app.listen(8000);
---
+++ users.js
@@ -0,0 +1,21 @@
+// dummy date to test the database
+
+var usersList = [
+  { name: 'Tin Vo',
+    email: 'tin@email.com',
+    password: '123',
+    address: '123 main st'
+  },
+  { name: 'Thuan Ng',
+    email: 'thuan@email.com',
+    password: '321',
+    address: '312 1st st'
+  },
+  { name: 'Yi Ba Lien',
+    email: 'balien@email.com',
+    password: '111',
+    address: '111 Ba Lien st'
+  }
+];
+
+module.exports.arr = usersList;
