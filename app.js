87d013acdc7a20468c740366e9ab8264dd5c713c
Tin Vo <tinnvo1101@gmail.com>
2017-04-08 11:39:58 -0500

    Added more dummy data for db (testing)

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ app.use(log('dev'));
 app.use(express.static(__dirname + '/static'));
 
 // check connection to postgres database
-var Sequelize = require('sequelize')
-  , sequelize = new Sequelize('postgres', 'postgres', '123456', {
-      dialect: "postgres",
-      port:    5432,
-    });
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres', 'postgres', '123456', {
+    dialect: "postgres",
+    port:    5432,
+});
 
 // define users table
 var Users = sequelize.define('Users', {
@@ -44,21 +44,21 @@ app.get('/', (req, res) => res.status(200).send({
 
 // add users to db - TESTING ONLY
 app.get('/add', function(req, res) {
-  sequelize
+    sequelize
     .sync({force:true})
     .then(function(err) {
-      usersList.forEach(function(user) {
-        var userInstance = Users.build({
-          name: user.name,
-          email: user.email,
-          password: user.password,
-          address: user.address
+        usersList.forEach(function(user) {
+            var userInstance = Users.build({
+                name: user.name,
+                email: user.email,
+                password: user.password,
+                address: user.address
+            });
+            userInstance.save();
         });
-        userInstance.save();
-      });
-      console.log('It worked.')
+        res.send('Successfully added users');
     }, function (err) {
-      console.log('An error occurred!!!', err);
+        console.log('An error occurred!!!', err);
     });
 });
 
@@ -68,33 +68,32 @@ app.get('/add', function(req, res) {
 //----------------------------------------//
 // get all users
 app.get('/api/users',function(req, res){
-  sequelize
+    sequelize
     .sync()
     .then(function(err) {
-      Users.findAll().then(function(users) {
-        console.log(users);
-        res.send(users);
-      });
+        Users.findAll().then(function(users) {
+            res.send(users);
+        });
     }, function (err) {
-      res.send('An error occurred!!!', err);
+        res.send('An error occurred!!!', err);
     });
 });
 
 // add a user to the database
 app.post('/api/users', function(req, res) {
-  sequelize
+    sequelize
     .sync()
     .then(function(err) {
-      var userInstance = Users.build({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        address: req.body.address
-      });
-      userInstance.save();
-      res.send('Successfully added a user.');
+        var userInstance = Users.build({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            address: req.body.address
+        });
+        userInstance.save();
+        res.send('Successfully added a user.');
     }, function (err) {
-      res.send('An error occurred!!!', err);
+        res.send('An error occurred!!!', err);
     });
 });
 
--- users.js
+++ users.js
@@ -1,31 +1,532 @@
 // dummy data to test the database
 
 var usersList = [
-  { name: 'Tin Vo',
-    email: 'tin@email.com',
-    password: '123',
-    address: '123 main st'
-  },
-  { name: 'Thuan Ng',
-    email: 'thuan@email.com',
-    password: '321',
-    address: '312 1st st'
-  },
-  { name: 'Yi Ba Lien',
-    email: 'balien@email.com',
-    password: '111',
-    address: '111 Ba Lien st'
-  },
-  { name: 'Tra Cho',
-    email: 'tra@email.com',
-    password: '222',
-    address: '222 Nga Tu Chuong Cho'
-  },
-  { name: 'Tuan Bo',
-    email: 'buffalo@email.com',
-    password: '3',
-    address: '3 Nga Tu Chuong Bo'
-  }
+    {
+      name: 'Tin Vo',
+      email: 'tin@email.com',
+      password: '123',
+      address: '123 main st'
+    },{
+      name: 'Thuan Ng',
+      email: 'thuan@email.com',
+      password: '321',
+      address: '312 1st st'
+    }, {
+      name: 'Yi Ba Lien',
+      email: 'balien@email.com',
+      password: '111',
+      address: '111 Ba Lien st'
+    },{
+      name: 'Tra Cho',
+      email: 'tra@email.com',
+      password: '222',
+      address: '222 Nga Tu Chuong Cho'
+    }, {
+      name: 'Tuan Bo',
+      email: 'buffalo@email.com',
+      password: '3',
+      address: '3 Nga Tu Chuong Bo'
+    },{
+      name: "Tammy Alexander",
+      email: "talexander0@blinklist.com",
+      password: "pmXo2nXbuP",
+      address: "8428 Lerdahl Avenue"
+    }, {
+      name: "Annie Henderson",
+      email: "ahenderson1@scientificamerican.com",
+      password: "h1lA2BsZWY",
+      address: "20 Loomis Pass"
+    }, {
+      name: "Virginia Morrison",
+      email: "vmorrison2@yahoo.co.jp",
+      password: "LopnvrhDlz",
+      address: "31 Buena Vista Parkway"
+    }, {
+      name: "Ryan Allen",
+      email: "rallen3@photobucket.com",
+      password: "HnFkHb",
+      address: "7755 Toban Pass"
+    }, {
+      name: "Joseph Clark",
+      email: "jclark4@homestead.com",
+      password: "7xokAJ7",
+      address: "7 Havey Point"
+    }, {
+      name: "Jason Vasquez",
+      email: "jvasquez5@chron.com",
+      password: "BnB3kAqJr11",
+      address: "477 Ridge Oak Circle"
+    }, {
+      name: "Bonnie Banks",
+      email: "bbanks6@bloglovin.com",
+      password: "jlRPXKJ",
+      address: "583 Sugar Drive"
+    }, {
+      name: "Matthew Peters",
+      email: "mpeters7@reddit.com",
+      password: "alswEPg1C",
+      address: "29 Green Ridge Lane"
+    }, {
+      name: "Juan Mccoy",
+      email: "jmccoy8@usda.gov",
+      password: "AELQEsiSA",
+      address: "86 Gina Hill"
+    }, {
+      name: "Nancy Mills",
+      email: "nmills9@lycos.com",
+      password: "7rmWhITT5dxo",
+      address: "5941 Carpenter Road"
+    }, {
+      name: "Kathryn Wright",
+      email: "kwrighta@imageshack.us",
+      password: "SjaeFmBJ",
+      address: "990 Maple Trail"
+    }, {
+      name: "Ronald Reyes",
+      email: "rreyesb@wordpress.org",
+      password: "NwV2pa7n3IM",
+      address: "3 Hansons Plaza"
+    }, {
+      name: "Kevin Diaz",
+      email: "kdiazc@europa.eu",
+      password: "WXJzZOT0x",
+      address: "097 Continental Trail"
+    }, {
+      name: "Sharon Dixon",
+      email: "sdixond@reuters.com",
+      password: "BUgcEQA9tF",
+      address: "87363 Transport Point"
+    }, {
+      name: "Todd Jacobs",
+      email: "tjacobse@multiply.com",
+      password: "QKJ8Ytlh",
+      address: "88922 Golden Leaf Alley"
+    }, {
+      name: "Linda Greene",
+      email: "lgreenef@xing.com",
+      password: "73n8DUV",
+      address: "992 Bunker Hill Crossing"
+    }, {
+      name: "Justin Washington",
+      email: "jwashingtong@indiatimes.com",
+      password: "XXZ56f4HsL0r",
+      address: "30652 Forest Dale Place"
+    }, {
+      name: "Gloria Richardson",
+      email: "grichardsonh@oracle.com",
+      password: "iJFZz5FlATFL",
+      address: "78 New Castle Alley"
+    }, {
+      name: "Sandra Brooks",
+      email: "sbrooksi@narod.ru",
+      password: "Skn1aAcXAq8G",
+      address: "239 Dorton Terrace"
+    }, {
+      name: "Jesse Nguyen",
+      email: "jnguyenj@shareasale.com",
+      password: "SGR4su",
+      address: "99725 Oneill Plaza"
+    }, {
+      name: "Deborah Cunningham",
+      email: "dcunninghamk@arstechnica.com",
+      password: "yXocLTKUPku",
+      address: "062 Milwaukee Way"
+    }, {
+      name: "Jennifer Carter",
+      email: "jcarterl@topsy.com",
+      password: "kjkj1aWO",
+      address: "34574 Monument Lane"
+    }, {
+      name: "Victor Hill",
+      email: "vhillm@umn.edu",
+      password: "1JE14xHPD92",
+      address: "874 Old Shore Alley"
+    }, {
+      name: "Kelly Cook",
+      email: "kcookn@miibeian.gov.cn",
+      password: "zYIaHVFH",
+      address: "5781 Pierstorff Road"
+    }, {
+      name: "Mary Boyd",
+      email: "mboydo@answers.com",
+      password: "q4jsMo8C",
+      address: "2 Orin Plaza"
+    }, {
+      name: "Sarah Collins",
+      email: "scollinsp@vkontakte.ru",
+      password: "inr71oiSV6O",
+      address: "708 Raven Court"
+    }, {
+      name: "Jerry Sanders",
+      email: "jsandersq@ucoz.com",
+      password: "825cycl",
+      address: "077 John Wall Lane"
+    }, {
+      name: "Carol Lane",
+      email: "claner@google.co.uk",
+      password: "F4AwDxC",
+      address: "724 Summerview Way"
+    }, {
+      name: "Lori Pierce",
+      email: "lpierces@wikipedia.org",
+      password: "3RKA8MN7rAQN",
+      address: "1580 Stephen Parkway"
+    }, {
+      name: "Henry Daniels",
+      email: "hdanielst@cisco.com",
+      password: "3OfRgbuguIXp",
+      address: "99 High Crossing Junction"
+    }, {
+      name: "Aaron Lawson",
+      email: "alawsonu@themeforest.net",
+      password: "OLRq5OdCPep",
+      address: "58 Green Ridge Place"
+    }, {
+      name: "Richard Bell",
+      email: "rbellv@ca.gov",
+      password: "ivlGi70",
+      address: "16 Caliangt Circle"
+    }, {
+      name: "Todd Gonzalez",
+      email: "tgonzalezw@sina.com.cn",
+      password: "SRs8Gk1k6N",
+      address: "85 Merry Street"
+    }, {
+      name: "Gerald Reyes",
+      email: "greyesx@flickr.com",
+      password: "spM2XvQST8dU",
+      address: "98563 Lunder Trail"
+    }, {
+      name: "Ralph Martin",
+      email: "rmartiny@uiuc.edu",
+      password: "RE0MY5w1GKsN",
+      address: "462 Lawn Way"
+    }, {
+      name: "Janet Morrison",
+      email: "jmorrisonz@blogspot.com",
+      password: "RMJZO1isD",
+      address: "950 Ilene Road"
+    }, {
+      name: "Aaron Simmons",
+      email: "asimmons10@alibaba.com",
+      password: "7z6DkZqEc",
+      address: "8 Schlimgen Alley"
+    }, {
+      name: "Russell Wood",
+      email: "rwood11@nymag.com",
+      password: "TeIn9pLNa",
+      address: "700 Moose Road"
+    }, {
+      name: "Walter Oliver",
+      email: "woliver12@rambler.ru",
+      password: "AXFc2z",
+      address: "85 Packers Crossing"
+    }, {
+      name: "Shawn George",
+      email: "sgeorge13@harvard.edu",
+      password: "aC5ap0m4H",
+      address: "075 Hoffman Center"
+    }, {
+      name: "Martha Kelly",
+      email: "mkelly14@army.mil",
+      password: "opGM2Qge",
+      address: "7 Schlimgen Parkway"
+    }, {
+      name: "Norma Welch",
+      email: "nwelch15@github.io",
+      password: "wgUDkNUednJ",
+      address: "211 Lerdahl Place"
+    }, {
+      name: "Stephen Hughes",
+      email: "shughes16@pbs.org",
+      password: "Mf1h7NjywL",
+      address: "9727 Helena Street"
+    }, {
+      name: "Alice Howard",
+      email: "ahoward17@earthlink.net",
+      password: "b56Jip",
+      address: "53 Victoria Pass"
+    }, {
+      name: "Kathy Nichols",
+      email: "knichols18@geocities.com",
+      password: "lpJTIbxB",
+      address: "4554 Delaware Drive"
+    }, {
+      name: "Steven Murray",
+      email: "smurray19@gmpg.org",
+      password: "ez9gYMP3",
+      address: "154 Birchwood Crossing"
+    }, {
+      name: "Lisa Willis",
+      email: "lwillis1a@networkadvertising.org",
+      password: "z6NTUxxl",
+      address: "185 Service Junction"
+    }, {
+      name: "Helen Austin",
+      email: "haustin1b@timesonline.co.uk",
+      password: "Joi6jZhAUm0V",
+      address: "23 Del Mar Park"
+    }, {
+      name: "Dorothy Lynch",
+      email: "dlynch1c@scribd.com",
+      password: "JBFX4X",
+      address: "2506 Quincy Way"
+    }, {
+      name: "George Price",
+      email: "gprice1d@dmoz.org",
+      password: "N6esduk",
+      address: "7 Roth Avenue"
+    }, {
+      name: "Anthony Hansen",
+      email: "ahansen1e@miitbeian.gov.cn",
+      password: "4e7mAc",
+      address: "342 Beilfuss Street"
+    }, {
+      name: "Brian Bowman",
+      email: "bbowman1f@qq.com",
+      password: "Sr1APct",
+      address: "2 Derek Trail"
+    }, {
+      name: "Ashley Collins",
+      email: "acollins1g@edublogs.org",
+      password: "dbtXLM",
+      address: "269 Fair Oaks Hill"
+    }, {
+      name: "Katherine Richardson",
+      email: "krichardson1h@cdc.gov",
+      password: "fSmHQ5f",
+      address: "98 Scott Trail"
+    }, {
+      name: "Andrea Hawkins",
+      email: "ahawkins1i@google.nl",
+      password: "Ntpa3R18",
+      address: "6 Bunker Hill Plaza"
+    }, {
+      name: "Anne Bryant",
+      email: "abryant1j@goodreads.com",
+      password: "0yT64VYJNbPn",
+      address: "7 Harper Park"
+    }, {
+      name: "Clarence Banks",
+      email: "cbanks1k@mysql.com",
+      password: "5yZmo4aV6r0A",
+      address: "474 Schiller Crossing"
+    }, {
+      name: "Robin Cole",
+      email: "rcole1l@dagondesign.com",
+      password: "uGD04IK",
+      address: "8 Elgar Terrace"
+    }, {
+      name: "Lori Watkins",
+      email: "lwatkins1m@japanpost.jp",
+      password: "luTVUmMj33x",
+      address: "7260 Anthes Alley"
+    }, {
+      name: "Jose Mitchell",
+      email: "jmitchell1n@telegraph.co.uk",
+      password: "hGZCrckBkiy",
+      address: "3084 Manitowish Point"
+    }, {
+      name: "Sharon Fisher",
+      email: "sfisher1o@google.nl",
+      password: "HYz2Myg",
+      address: "7827 Jenna Road"
+    }, {
+      name: "Carol Butler",
+      email: "cbutler1p@biglobe.ne.jp",
+      password: "a7RchEjUccrZ",
+      address: "2154 Fuller Trail"
+    }, {
+      name: "Matthew Young",
+      email: "myoung1q@sciencedirect.com",
+      password: "WKxTGgc",
+      address: "374 Anthes Park"
+    }, {
+      name: "Timothy Greene",
+      email: "tgreene1r@wikimedia.org",
+      password: "X0VI6Z",
+      address: "500 Colorado Drive"
+    }, {
+      name: "Carolyn Sanchez",
+      email: "csanchez1s@amazonaws.com",
+      password: "Nz2BtTZcYPu",
+      address: "9484 Melvin Street"
+    }, {
+      name: "Eugene Grant",
+      email: "egrant1t@deliciousdays.com",
+      password: "9GohUta",
+      address: "4721 Mosinee Hill"
+    }, {
+      name: "Karen Castillo",
+      email: "kcastillo1u@shareasale.com",
+      password: "YeyH1nmNNa",
+      address: "3106 Sundown Place"
+    }, {
+      name: "Walter Ford",
+      email: "wford1v@domainmarket.com",
+      password: "YptVwjj",
+      address: "3 Corscot Crossing"
+    }, {
+      name: "Kathryn Wilson",
+      email: "kwilson1w@narod.ru",
+      password: "KqIPoNT",
+      address: "72 Green Ridge Court"
+    }, {
+      name: "Maria Alexander",
+      email: "malexander1x@hp.com",
+      password: "FFMghdTylLi",
+      address: "33 Loftsgordon Park"
+    }, {
+      name: "Jesse Harris",
+      email: "jharris1y@godaddy.com",
+      password: "K8gxY7",
+      address: "655 Vernon Point"
+    }, {
+      name: "Debra Cook",
+      email: "dcook1z@canalblog.com",
+      password: "ftf5pLu",
+      address: "23749 Morning Way"
+    }, {
+      name: "Joshua Fernandez",
+      email: "jfernandez20@123-reg.co.uk",
+      password: "0eEiJgyougZ",
+      address: "7 Gulseth Road"
+    }, {
+      name: "Sara Cooper",
+      email: "scooper21@gov.uk",
+      password: "7UDhgdYegnAg",
+      address: "59164 Rigney Lane"
+    }, {
+      name: "Judith George",
+      email: "jgeorge22@parallels.com",
+      password: "EBVT5LU",
+      address: "5346 Porter Trail"
+    }, {
+      name: "Janet Henderson",
+      email: "jhenderson23@desdev.cn",
+      password: "cGXKaRU",
+      address: "37 Union Lane"
+    }, {
+      name: "Dennis Elliott",
+      email: "delliott24@nba.com",
+      password: "TNUpuozTeiVw",
+      address: "68 Lukken Avenue"
+    }, {
+      name: "Lois Gutierrez",
+      email: "lgutierrez25@infoseek.co.jp",
+      password: "VF2xQihoENK",
+      address: "2 Evergreen Circle"
+    }, {
+      name: "Larry Rose",
+      email: "lrose26@chronoengine.com",
+      password: "6tC73BIcnmPu",
+      address: "21 Linden Junction"
+    }, {
+      name: "Samuel Thomas",
+      email: "sthomas27@istockphoto.com",
+      password: "y4UvazIHNoI",
+      address: "950 Buell Terrace"
+    }, {
+      name: "Sara Johnson",
+      email: "sjohnson28@narod.ru",
+      password: "evBD0tOwuW",
+      address: "17945 Kropf Crossing"
+    }, {
+      name: "Jason Hamilton",
+      email: "jhamilton29@businesswire.com",
+      password: "Smb6iP9jn",
+      address: "91375 Armistice Hill"
+    }, {
+      name: "Edward Cook",
+      email: "ecook2a@privacy.gov.au",
+      password: "jCuHmzj",
+      address: "0901 Village Green Alley"
+    }, {
+      name: "Carl Webb",
+      email: "cwebb2b@mozilla.com",
+      password: "Ytfox0Aa",
+      address: "9098 Rusk Drive"
+    }, {
+      name: "Joshua Rogers",
+      email: "jrogers2c@paginegialle.it",
+      password: "vojcltI",
+      address: "6 Warner Plaza"
+    }, {
+      name: "Kathryn Vasquez",
+      email: "kvasquez2d@mapy.cz",
+      password: "viT3E7Bjx",
+      address: "3 Bultman Center"
+    }, {
+      name: "Antonio Webb",
+      email: "awebb2e@eventbrite.com",
+      password: "MLeFpPhj",
+      address: "54 Express Junction"
+    }, {
+      name: "Janet Bryant",
+      email: "jbryant2f@pinterest.com",
+      password: "6ipA3ezEN",
+      address: "022 Golf Place"
+    }, {
+      name: "Rachel Gonzalez",
+      email: "rgonzalez2g@ibm.com",
+      password: "wLXUNidtAE0A",
+      address: "8 Oriole Street"
+    }, {
+      name: "Bobby Fuller",
+      email: "bfuller2h@usatoday.com",
+      password: "3FTzhV",
+      address: "3 2nd Pass"
+    }, {
+      name: "Judy Howell",
+      email: "jhowell2i@sbwire.com",
+      password: "aCpvhqWY",
+      address: "354 Mayfield Center"
+    }, {
+      name: "Stephen Stone",
+      email: "sstone2j@hao123.com",
+      password: "FDafwDIUQmyb",
+      address: "37 La Follette Hill"
+    }, {
+      name: "Gary Vasquez",
+      email: "gvasquez2k@state.tx.us",
+      password: "V4ifWFIJ",
+      address: "648 Eagle Crest Drive"
+    }, {
+      name: "Debra West",
+      email: "dwest2l@vkontakte.ru",
+      password: "cKtpmTBzBxj",
+      address: "64 Florence Way"
+    }, {
+      name: "Kimberly Howell",
+      email: "khowell2m@ehow.com",
+      password: "WYYk9hY8vCmb",
+      address: "3970 Bartillon Hill"
+    }, {
+      name: "Howard Murphy",
+      email: "hmurphy2n@arizona.edu",
+      password: "AN16i5jNpgnl",
+      address: "70 Independence Road"
+    }, {
+      name: "Andrew Stanley",
+      email: "astanley2o@reverbnation.com",
+      password: "g5atbCbk",
+      address: "001 Valley Edge Park"
+    }, {
+      name: "Eric Gonzales",
+      email: "egonzales2p@privacy.gov.au",
+      password: "PPiDc0AXNGyP",
+      address: "18 Mallard Circle"
+    }, {
+      name: "Louis Lynch",
+      email: "llynch2q@guardian.co.uk",
+      password: "3Uxb8422D",
+      address: "3838 Shelley Point"
+    }, {
+      name: "Shirley Perez",
+      email: "sperez2r@china.com.cn",
+      password: "NyrgzE2Imtik",
+      address: "156 Summit Alley"
+    }
 ];
 
 module.exports.arr = usersList;
